608bc123d193768f291403b7b78cbd4d6f58f3ca
judith <judithoguche@gmail.com>
2022-09-08 15:55:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main-serves as the entry point of the program
+*
+*Return:by default, returns zero if no errors otherwise returns non-zero value.
+*/
+int main()
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}

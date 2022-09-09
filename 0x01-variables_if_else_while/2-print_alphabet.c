c6f462c2328c39911d2d2fe3024f0e073f8d6c93
judith <judithoguche@gmail.com>
2022-09-09 16:33:15 -0700

    Program that prints lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - print if the number is positive, negative or zero
+*Description: this program  prints the alphabet in lowercase, followed by a new line
+*Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);

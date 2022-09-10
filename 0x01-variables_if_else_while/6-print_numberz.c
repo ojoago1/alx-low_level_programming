e1d820e4ae13ac0f597ecf1bd1ce0ae99310ff5b
judith <judithoguche@gmail.com>
2022-09-10 13:32:57 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Enrty point
+*
+*Description: a program that prints all single digit numbers of base 10 starting from 0, followed by a new line
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);

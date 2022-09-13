3b47bdc3fc6ae3850ce9d7579a3f6fb8b577e956
judith <judithoguche@gmail.com>
2022-09-13 15:46:15 -0700

    print 10 times alphabet in lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,22 +2,30 @@
 
 /**
 
- * add - Adds two integers.
-
- * @num1: the first integer to be added
-
- * @num2: the second integer to be added
-
- *
-
- * Return: The result of the addition.
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase.
 
  */
 
-int add(int num1, int num2)
+void print_alphabet_x10(void)
 
 {
 
-	return (num1 + num2);
+	int count = 0;
+
+	char n;
+
+
+
+	while (count++ <= 9)
+
+	{
+
+		for (n = 'a'; n <= 'z'; n++)
+
+		_putchar(n);
+
+		_putchar('\n');
+
+	}
 
 }

89ac6a79b4b1c4f92b7f28ed013992205bfb3dc2
Judith Oguche <judithoguche@gmail.com>
2022-09-15 23:07:15 +0100

    is upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - checks for uppercase
- *
- * Return: always 0
- * /
-int main(void)
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 if sucessful
+ */
+
+int _isupper(int c)
 {
 	if (c >= 65 && c < 91)
 	{

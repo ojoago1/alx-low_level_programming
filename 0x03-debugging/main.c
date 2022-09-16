a989f85b581d09a0a2d7438aa59f51146639e872
judith <judithoguche@gmail.com>
2022-09-16 16:07:52 -0700

    largest

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -13,6 +13,7 @@ int convert_day(int month, int day)
 int largest_number(int a, int b, int c)
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c)
 
 
 

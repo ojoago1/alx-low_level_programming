0eff953ed000694eadae4d25dfbc6d2149e95be7
judith <judithoguche@gmail.com>
2022-09-18 10:16:15 -0700

    test

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,18 +3,9 @@
 #define MAIN_H
 
 
-
 #include <stdio.h>
 
-void print_remaining_days(int month, int day, int year)
-
-int convert_day(int month, int day)
-
-int largest_number(int a, int b, int c)
-
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
 
 
-
-#endif /* MAIN_H */
+#endif /* MAIN_H */i

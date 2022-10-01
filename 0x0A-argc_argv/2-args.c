635c2a8d2a779092729bb344359b0e4879b41695
Judith Oguche <judithoguche@gmail.com>
2022-10-01 04:39:22 +0100

    program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,1 +1,17 @@
-hdhj
+#include "stdio.h"
+/**
+ * main - prints all arguments
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i = 0;
+while (argc--)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}

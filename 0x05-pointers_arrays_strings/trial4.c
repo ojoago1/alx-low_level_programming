c49c739ac1fc56f6bb1ce0de11c3d8b3ab940393
judith <judithoguche@gmail.com>
2022-09-30 21:51:51 -0700

    putchar

---
+++ 0x05-pointers_arrays_strings/trial4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - to get the address of pointer
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int *p;
+
+	printf("Address of variable 'p': %p\n", &p);
+	return (0);
+}

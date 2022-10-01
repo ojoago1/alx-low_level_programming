db2e5dfa4463a7a4037067f219609513223a9981
Judith Oguche <judithoguche@gmail.com>
2022-10-01 05:53:15 +0100

    _putchar.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,27 +1,14 @@
 #include <unistd.h>
 
-
-
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and error is there returned
- *
- *       */
-
-
+ * Return: On success 1.
+ * On error, -1 is returned, and error is there returned
+ */
 
 int _putchar(char c)
-
 {
-
-	return (write(1, &c, 1));
-
+return (write(1, &c, 1));
 }

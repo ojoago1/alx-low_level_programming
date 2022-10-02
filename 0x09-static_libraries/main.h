32b4c85dce2aa9026d75f39c5a833dd304aa23cf
judith <judithoguche@gmail.com>
2022-10-02 10:58:42 -0700

    libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,5 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
-
 
 int _putchar(char c);
 
@@ -43,7 +40,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
-
-
 
 #endif

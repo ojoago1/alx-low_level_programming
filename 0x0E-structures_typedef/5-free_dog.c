8e17c0dab59d9ddc24887ae86741a9710129f6ff
Judith Oguche <judithoguche@gmail.com>
2022-10-10 23:27:51 +0100

    function that frees dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,1 +1,18 @@
-jbhdj
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog memory
+ * @d: dog
+ */
+
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+	return;
+
+free(d->owner);
+free(d->name);
+free(d);
+}

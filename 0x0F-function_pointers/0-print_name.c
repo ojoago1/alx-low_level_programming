b3581fed980b25cb3608b555480b13288c6b9dfe
Judith Oguche <judithoguche@gmail.com>
2022-10-14 21:21:15 +0100

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,13 @@
-hjhj
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: string
+ * @f: function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f != NULL)
+	f(name);
+}

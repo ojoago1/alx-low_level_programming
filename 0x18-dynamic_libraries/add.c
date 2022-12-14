c049796ad740f11f32320ca9cfe6197ed4c921a4
ojoago1 <judithoguche@gmail.com>
2022-12-14 02:51:44 -0800

    library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+
+ * add - adds two integers
+
+ * @a: integer to be added
+
+ * @b: integer to be added
+
+ * Returns: returns the addition of a and b
+
+ */
+
+int add(int a, int b)
+
+{
+
+	return (a + b);
+
+}
